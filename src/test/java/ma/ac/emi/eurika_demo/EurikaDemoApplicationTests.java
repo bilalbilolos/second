9e2ef45d566e56347b4187d80cdf62ae9e068d46
BMA <mahdaaoui@gmail.com>
2019-12-05 10:44:31 +0100

    this is the second one

---
+++ src/test/java/ma/ac/emi/eurika_demo/EurikaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ac.emi.eurika_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurikaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
